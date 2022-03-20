6650a0b6e32fa25e342b7135533a8e92f2d7644d
Nahian Ahmed <nahianahmedcse@gmail.com>
2022-03-20 21:33:55 +0600

    optimize import

--- src/main/java/com/nahian/github/io/rookiedev/RookieDevApplication.java
+++ src/main/java/com/nahian/github/io/rookiedev/RookieDevApplication.java
@@ -1,15 +1,8 @@
 package com.nahian.github.io.rookiedev;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableCaching
